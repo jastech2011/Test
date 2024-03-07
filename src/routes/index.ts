603b3b5bd18fae574b6398c17ecc428eebbea22a
Serhii-Kondriev <jastech2011@gmail.com>
2024-03-07 20:18:36 +0200

    init

--- src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import express, { Request, Response, Router } from "express";
-import { getFilteredResponses } from "../controllers/responsesController";
-
-const router: Router = express.Router();
-
-router.get("/:formId/filteredResponses", getFilteredResponses);
-
-export default router;
